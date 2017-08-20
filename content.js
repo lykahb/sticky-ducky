ea52e82474299434ab77e87dd326de903471b5e7
Boris Lykah <lykahb@gmail.com>
2017-08-19 21:17:59 -0400

    ES6

--- content.js
+++ content.js
@@ -1,10 +1,10 @@
 // TODO: do not hide header when it would be regularly shown
 // Detect reactive frameworks by listening to mutations
 // Create unique CSS path to the element and inject style so that it is not modified
-var isDevelopment = true;
+const isDevelopment = true;
 
-var exploredHeaders = [];
-let selectorGenerator = new CssSelectorGenerator()
+let exploredHeaders = [];
+let selectorGenerator = new CssSelectorGenerator();
 let styleFixElement = null;
 
 function log(...args) {
@@ -13,17 +13,17 @@ function log(...args) {
     }
 }
 
-log('Script loaded')
+log('Script loaded');
 
 function isFixed(el) {
-    return window.getComputedStyle(el).position == "fixed";
+    return window.getComputedStyle(el).position === "fixed";
 }
 
 function classify(el) {
     // header, footer, splash, widget, sidebar
-    var rect = el.getBoundingClientRect();
-    var clientWidth = document.documentElement.clientWidth;
-    var clientHeight = document.documentElement.clientHeight;
+    const rect = el.getBoundingClientRect(),
+        clientWidth = document.documentElement.clientWidth,
+        clientHeight = document.documentElement.clientHeight;
 
     if (rect.width / clientWidth > 0.25) {
         if (rect.height / clientHeight < 0.25) {
@@ -48,8 +48,8 @@ function elementFromPoint(x, y, isPercent) {
 }
 
 function parentChain(el, pred) {
-    if (el.tagName != "HTML") {
-        while (el.tagName != "BODY") {
+    if (el.tagName !== "HTML") {
+        while (el.tagName !== "BODY") {
             if (pred(el)) {
                 return el;
             }
@@ -59,12 +59,12 @@ function parentChain(el, pred) {
 }
 
 function exploreInVicinity(el) {
-    var rect = el.getBoundingClientRect();
-    var middleX = rect.left + rect.width / 2;
-    var middleY = rect.top + rect.height / 2;
+    const rect = el.getBoundingClientRect(),
+        middleX = rect.left + rect.width / 2,
+        middleY = rect.top + rect.height / 2,
 
-    var coords = [[middleX, rect.top - 5], [middleX, rect.bottom + 5],
-        [rect.left - 5, middleY], [rect.right + 5, middleY], [middleX, middleY]];
+        coords = [[middleX, rect.top - 5], [middleX, rect.bottom + 5],
+            [rect.left - 5, middleY], [rect.right + 5, middleY], [middleX, middleY]];
 
     return coords.map(([x, y]) => elementFromPoint(x, y)).filter(Boolean);
 }
@@ -73,7 +73,7 @@ function fixElementOpacity(els) {
     // Specificity is too low if other classes override opacity
     $(els).addClass("RemoveHeaderExtension");
     // In case the header has animation keyframes involving opacity
-    $(els).filter((_, el) => $(el).css("animation")).css("animation", "none")
+    $(els).filter((_, el) => $(el).css("animation")).css("animation", "none");
     // Check if opacity is directly in style
     $(els).filter((_, el) => el.style.opacity).css("opacity", "")
 }
@@ -82,8 +82,8 @@ function fixElementFixed(els) {
     if (!els.length) {
         return;
     }
-    let selectors = els.map(el => selectorGenerator.getSelector(el));
-    css = selectors.join(',') + ' { position: static !IMPORTANT; }',
+    let selectors = els.map(el => selectorGenerator.getSelector(el)),
+        css = `${selectors.join(',')} { position: static !IMPORTANT; }`,
         head = document.head || document.getElementsByTagName('head')[0],
         style = document.createElement('style');
     if (styleFixElement) {
@@ -97,19 +97,19 @@ function fixElementFixed(els) {
 
 
 function doAll() {
-    log("scrolled")
+    log("scrolled");
     let foundNewInVicinity = false;
 
     function explore(headers) {
-        for (var i = 0; i < headers.length; i++) {
-            var header = headers[i];
-            var explored = _.uniq(filterHeaders(exploreInVicinity(header)));
-            var newHeaders = _.difference(explored, headers)
+        for (const header of headers) {
+            const explored = _.uniq(filterHeaders(exploreInVicinity(header))),
+                newHeaders = _.difference(explored, headers);
             if (newHeaders.length > 0) {
                 foundNewInVicinity = true;
                 headers = headers.concat(newHeaders);
             }
         }
+
         return headers;
     }
 
@@ -117,13 +117,13 @@ function doAll() {
         return headers.map(el => el && parentChain(el, isFixed)).filter(Boolean)
     }
 
-    if (exploredHeaders.length == 0) {
+    if (exploredHeaders.length === 0) {
         // There may be several fixed headers, one below another, and directly under (z-indexed).
-        var topRow = _.range(0, 1, 0.1).map(x => [x, 0.01]);
-        var bottomRow = _.range(0, 1, 0.1).map(x => [x, 0.95]);
-        var allCoords = topRow.concat(bottomRow);
-        var initial = _.uniq(allCoords.map(([x, y]) => elementFromPoint(x, y, true)));
-        log("Checking " + initial.length + " elements")
+        let topRow = _.range(0, 1, 0.1).map(x => [x, 0.01]),
+            bottomRow = _.range(0, 1, 0.1).map(x => [x, 0.95]),
+            allCoords = topRow.concat(bottomRow),
+            initial = _.uniq(allCoords.map(([x, y]) => elementFromPoint(x, y, true)));
+        log(`Checking ${initial.length} elements`);
         exploredHeaders = _.uniq(filterHeaders(initial));
     }
     [exploredHeaders, removed] = _.partition(exploredHeaders, el => document.body.contains(el));
@@ -135,22 +135,22 @@ function doAll() {
     exploredHeaders = explore(exploredHeaders);
 
     // Some explored headers are no longer fixed, retain them just in case
-    let toFix = exploredHeaders.filter(isFixed).filter(el => classify(el) != "sidebar");
+    let toFix = exploredHeaders.filter(isFixed).filter(el => classify(el) !== "sidebar");
     fixElementOpacity(toFix);
     log(exploredHeaders);
 }
 
-if (document.readyState == "interactive" || document.readyState == "complete") {
+if (document.readyState === "interactive" || document.readyState === "complete") {
     doAll();
 } else {
     document.addEventListener('DOMContentLoaded', doAll, false);
 }
 
 // TODO: repeat doAll on scroll until one screen is scrolled
-function onScroll(e) {
+function onScroll() {
     if (window.scrollY > document.documentElement.clientHeight) {
         // Assume that the dynamic header appeared and was processed once the document has been scrolled far enough
-        log("Scrolled far")
+        log("Scrolled far");
         window.removeEventListener("scroll", onScroll, false);
     }
     doAll();
