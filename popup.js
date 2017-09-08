cba58c1a3eaf691d6e2b3fd383493817ed4d2efb
Boris Lykah <lykahb@gmail.com>
2017-09-08 19:27:05 -0400

    Simplify popup

--- popup.js
+++ popup.js
@@ -1,16 +1,11 @@
-let currentBehavior = 'hover';
 chrome.storage.local.get('behavior', response => {
-    currentBehavior = response.behavior || currentBehavior;
-    getOption(currentBehavior).classList.add('active');
+    let behavior = response.behavior || 'hover';
+    document.querySelector(`#options > span[data-behavior=${behavior}]`).classList.add('active');
+    document.querySelectorAll('#options > span').forEach(el => el.addEventListener('click', e => {
+        let newBehavior = e.target.dataset.behavior;
+        if (newBehavior !== behavior) {
+            chrome.runtime.sendMessage({'behavior': newBehavior});
+        }
+        window.close();
+    }));
 });
-let getOption = behavior => document.querySelector(`#options > span[data-behavior=${currentBehavior}]`);
-
-document.querySelectorAll('#options > span').forEach(el => el.addEventListener('click', e => {
-    let newBehavior = e.target.dataset.behavior;
-    if (newBehavior && currentBehavior !== newBehavior) {
-        getOption(currentBehavior).classList.remove('active');
-        getOption(newBehavior).classList.add('active');
-        chrome.runtime.sendMessage({'behavior': newBehavior});
-    }
-    window.close();
-}));
