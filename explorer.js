799110ef45d32ec799ba3119ee3beced90a707cd
Boris Lykah <lykahb@gmail.com>
2018-09-03 17:34:06 -0400

    More robust handling of data urls

--- explorer.js
+++ explorer.js
@@ -3,6 +3,8 @@
 // This liberal comparison picks up "FiXeD !important" or "-webkit-sticky" positions
 let isFixedPos = p => p.toLowerCase().indexOf('fixed') >= 0 || p.toLowerCase().indexOf('sticky') >= 0;
 
+let isDataURL = url => /^\s*data:/i.test(url);
+
 // These functions recursively call each other. It is important both to handle the failures and process
 // all rules even after a failure. So the functions return promises that always resolve.
 
@@ -24,25 +26,35 @@ class Explorer {
         });
     }
 
-    exploreRules(rules, baseURI) {
+    exploreRules(rules) {
         for (let rule of rules) {
             if (rule.type === CSSRule.STYLE_RULE && isFixedPos(rule.style.position)) {
                 this.selectors.push(rule.selectorText);
             } else if (rule.type === CSSRule.MEDIA_RULE || rule.type === CSSRule.SUPPORTS_RULE) {
-                this.exploreRules(rule.cssRules, baseURI);
+                this.exploreRules(rule.cssRules);
             } else if (rule.type === CSSRule.IMPORT_RULE && rule.styleSheet) {
-                this.exploreStylesheet({sheet: rule.styleSheet});
+                this.exploreStylesheet(rule.styleSheet);
             } else if (rule.type === CSSRule.IMPORT_RULE && rule.href) {
-                this.fetchStylesheet(rule.href, baseURI);
+                this.fetchStylesheet(rule.href, this.getBaseURI(rule.parentStyleSheet, false));
             }
         }
     }
 
+    getBaseURI(sheet, isParent) {
+        return isParent && sheet.href && !isDataURL(sheet.href) && sheet.href
+            || sheet.ownerNode && sheet.ownerNode.baseURI
+            || this.getBaseURI(sheet.parentStyleSheet, true);
+    }
+
     fetchStylesheet(href, baseURI) {
         // Href may be relative, absolute or data url (https://bugs.chromium.org/p/chromium/issues/detail?id=813826)
-        let iframe;
-        let url = new URL(href, baseURI);
-        let promise = fetch(url, {method: 'GET', cache: 'force-cache'})
+        let iframe, absoluteURL, nestedBaseURI;
+        if (isDataURL(href)) {
+            [absoluteURL, nestedBaseURI] = [href, baseURI];
+        } else {
+            absoluteURL = nestedBaseURI = new URL(href, baseURI).href;
+        }
+        let promise = fetch(absoluteURL, {method: 'GET', cache: 'force-cache'})
             .then(response => response.ok ? response.text() : Promise.reject('Bad response'))
             .then(text => {
                 iframe = document.createElement('iframe');
@@ -50,30 +62,26 @@ class Explorer {
                 document.body.appendChild(iframe);
                 let iframeDoc = iframe.contentDocument;
                 // We need base for @import with relative urls. BaseURI may be on a different domain than href.
-                let base = url.protocol === 'data:' ? baseURI : url.href;
-                iframeDoc.head.appendChild(iframeDoc.createElement('base')).href = base;
+                iframeDoc.head.appendChild(iframeDoc.createElement('base')).href = nestedBaseURI;
                 let style = iframeDoc.head.appendChild(iframeDoc.createElement('style'));
                 style.textContent = text;
-                return this.exploreRules(style.sheet.cssRules, base);
+                return this.exploreRules(style.sheet.cssRules);
             })
             .catch(err => this.onFetchFail(href, baseURI, err))
             .then(() => iframe && iframe.remove());
         this.promises.push(promise);
     }
 
-    exploreStylesheet(sheetInfo) {
-        let sheet = sheetInfo.sheet;
+    exploreStylesheet(sheet) {
         let cssRules = null;
         try {
             cssRules = sheet.cssRules;
         } catch (e) {
         }
-        let baseURI = sheet.ownerNode && sheet.ownerNode.baseURI;
         if (cssRules) {
-            sheetInfo.rulesCount = cssRules.length;
-            this.exploreRules(cssRules, baseURI);
+            this.exploreRules(cssRules);
         } else if (sheet.href) {
-            this.fetchStylesheet(sheet.href, baseURI);
+            this.fetchStylesheet(sheet.href, this.getBaseURI(sheet, false));
         }
     }
 }
